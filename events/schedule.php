0706187afe0b2a155cefc10836458497b99ab6e4
shang0001@gmail.com <shang0001@gmail.com@9fc8e268-56b3-b768-80a5-42834bdd2e5a>
2013-01-30 04:58:48 +0000

    1. Reuse schedule.php as the data source of schedule.html, all events are stored in events.csv.
    2. Modify About Us sub-menus
    
    3. Page layout: move the "footer-wrapper" div into the container div

--- events/schedule.php
+++ events/schedule.php
@@ -1,9 +1,38 @@
-<?php 
-	$year = date('Y');
-	$month = date('M');
+<?php
+  $row = 1;
+  $captionArray = null;
+  $events = array();
+  if (($handle = fopen("events.csv", "r")) !== FALSE) {
+      while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+          $num = count($data);
+          if ($row == 1) {
+            $captionArray = $data;
+            //echo json_encode($captionArray);
+          }
+          else{
+            $event = array();
+            for ($c=0; $c < $num; $c++) {
+              if ($captionArray[$c] == "allDay"){
+                $event[$captionArray[$c]] = $data[$c] === 'true'? true: false;
+              }
+              else{
+                $event[$captionArray[$c]] = $data[$c];
+              }
+            }
+            //echo json_encode($event);
+            $events[] = $event;
+          }
+          $row++;
+      }
+      fclose($handle);
+  }
+  echo json_encode($events);
+
+	//$year = date('Y');
+	//$month = date('M');
 		
-	$event1 = array('id' => 1, 'title' => "Spring Festival", 'start' => "$year-$month-10", 'url' => "http://www.google.com");
-	$event2 = array('id' => 2, 'title' => "Christmas Party", 'start' => "$year-$month-24", 'url' => "http://www.baidu.com");
-	$events = array($event1, $event2);
-	echo json_encode($events);
+	//$event1 = array('id' => 1, 'title' => "Spring Festival", 'start' => "2013-01-04", 'url' => "http://www.google.com");
+	//$event2 = array('id' => 2, 'title' => "Christmas Party", 'start' => "2013-01-24", 'url' => "http://www.baidu.com");
+	//$eventss = array($event1, $event2);
+	//echo json_encode($eventss);
 ?>
