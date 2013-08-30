a172d02466089911af9aaf4f6729040f1492143f
marshall628@gmail.com <marshall628@gmail.com@9fc8e268-56b3-b768-80a5-42834bdd2e5a>
2013-08-30 02:48:08 +0000

    Update the annual general meeting page and put the current year into the footer

---
+++ trunk/management/chineseschool/search.php
@@ -0,0 +1,64 @@
+<?php
+//session_start();
+//$counter = isset($_SESSION['counter']) ? $_SESSION['counter'] : 0;
+//$counter++;
+//print "You have visited this page $counter times during this session";
+//$_SESSION['counter'] = $counter;
+
+$lang = $_GET["lang"];
+$filename = 'report_' . $lang . '.html';
+$curYear = date('Y');
+
+$years = $_POST['years'];
+$courses = $_POST['courses'];
+$members = $_POST['members'];
+
+if (file_exists($filename)) {
+    $xml = simplexml_load_file($filename);
+ 
+    //print_r($xml);
+} else {
+    echo 'Failed to open $filename';
+}
+
+// Create connection
+$con=mysqli_connect("ccanbca.ipagemysql.com","ccanb","ccanbadmin","ccanb");
+		
+// Check connection
+if (mysqli_connect_errno($con))
+{
+	echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+$courseregistrations = mysqli_query($con,"SELECT * FROM courseregistration where courseyear like '%$years' 
+		and course like '%$courses' and membership like '%$members'");
+while ($coursereg = mysqli_fetch_array($courseregistrations))
+{
+	$newRow = $xml->table->addChild('tr');
+	$newRow->addChild('td', $coursereg['sname']);
+	$newRow->addChild('td', $coursereg['email']);
+	$newRow->addChild('td', $coursereg['birthday']);
+	$newRow->addChild('td', $coursereg['medicare']);
+	$newRow->addChild('td', $coursereg['address']);
+	$newRow->addChild('td', $coursereg['postalcode']);
+	$newRow->addChild('td', $coursereg['hphone']);
+	$newRow->addChild('td', $coursereg['ophone']);
+	$newRow->addChild('td', $coursereg['cphone']);
+	$newRow->addChild('td', $coursereg['pname']);
+	$newRow->addChild('td', $coursereg['courseyear']);
+  $courses = split(',', $coursereg['course']);
+  $td = $newRow->addChild('td');
+  foreach ($courses as $course)
+  {
+    $td->addChild('div', $course);
+  }
+	$newRow->addChild('td', '$' . $coursereg['courseprice']);
+	$newRow->addChild('td', $coursereg['registrationtime']);
+	$newRow->addChild('td', ($coursereg['membership'] == 1 ? 'Yes' : 'No'));
+}
+
+mysqli_close($con);
+
+echo $xml->asXML();
+
+?>
